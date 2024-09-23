0455d5ba9f894810248689cb1dd1e510630245a5
fraserivison <fraserivison@hotmail.co.uk>
2024-09-23 11:23:19 +0000

    Added __str__ method to Recipe model for better admin display

--- recipes/models.py
+++ recipes/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
-class Post(models.Model):
+class Recipe(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
@@ -13,8 +13,5 @@ class Post(models.Model):
     STATUS = ((0, "Draft"), (1, "Published"))
     status = models.IntegerField(choices=STATUS, default=0)
 
-
-
-
-
-
+    def __str__(self):
+        return self.title
