39c6959eaf4a47b1675921ac57b12ab27abe16bb
fraserivison <fraserivison@hotmail.co.uk>
2024-09-27 22:54:12 +0000

    Integrate Cloudinary for image management in the Recipe model

--- sizzle/settings.py
+++ sizzle/settings.py
@@ -13,6 +13,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+# Cloudinary configuration
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'), 
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'))
 
 if os.path.isfile('env.py'):
     import env
