259bc9080851cfc5c437280f8e94a4a09abb4cb1
fraserivison <fraserivison@hotmail.co.uk>
2024-10-01 15:22:54 +0000

    Fix: Import redirect to handle recipe submission successfully

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from allauth.account.views import LoginView as AllauthLoginView, SignupView as AllauthSignupView, LogoutView as AllauthLogoutView
